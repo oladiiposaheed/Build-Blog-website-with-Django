4899a8803f2701aa16328c2056244d8218e10f45
Saheed <oladiiposaheed@gmail.com>
2025-08-30 16:27:46 +0100

    adding admin, views, templates, static and update the models and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404
+from blog.models import Post
+from django.http import Http404
 # Create your views here.
+
+# Ccreating a view to display the list of posts.
+def post_list(request):
+    posts = Post.published.all()
+    dict = {'posts': posts}
+    return render(request, 'blog/post/list.html', context=dict)
+
+#  Create a second view to display a single post.
+def post_detail(request, id):
+    post = get_list_or_404(Post, id=id, status= Post.Status.PUBLISHED)
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post found.')
+    
+    dict = {'post': post}
+    return render(request, 'blog/post/detail.html', context=dict)
